a77e74b006eb3953c1d13a29f4f0781a8cec10c6
nazwazhrr <nazwaazahra883@gmail.com>
2025-05-22 10:06:09 +0700

    Percobaan 1

---
+++ DoubleLinkedList01.java
@@ -0,0 +1,85 @@
+public class DoubleLinkedList01 {
+    Node01 head;
+    Node01 tail;
+
+    public DoubleLinkedList01() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa01 data) {
+        Node01 newNode = new Node01(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa01 data) {
+        Node01 newNode = new Node01(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    } 
+
+    public Node01 search(String nim) {
+        Node01 current = head;
+        while (current != null) {
+            if (current.data.nim.equals(nim)) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    public void insertAfter(String keyNim, Mahasiswa01 data) {
+        Node01 current = head;
+
+        // Cari node dengan nim = keyNim
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Node dengan NIM " + keyNim + " tidak ditemukan");
+            return;
+        }
+
+        Node01 newNode = new Node01(data);
+
+        // Jika current adalah tail, cukup tambahkan di akhir
+        if (current == tail) {
+            current.next = newNode;
+            newNode.prev = current;
+            tail = newNode;
+        } else {
+            // Sisipkan di tengah
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+
+        System.out.println("Node berhasil disisipkan setelah NIM " + keyNim);
+    }
+
+    public void print() {
+        Node01 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+}
